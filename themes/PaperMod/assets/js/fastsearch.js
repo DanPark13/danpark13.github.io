dd8ddd831c0de6c64493830e67a3c9149a9fef2d
DanPark13 <dpark0703@gmail.com>
2022-08-23 20:14:36 -0400

    Attempt 1 - Fix fastsearch

--- themes/PaperMod/assets/js/fastsearch.js
+++ themes/PaperMod/assets/js/fastsearch.js
@@ -48,7 +48,8 @@ window.onload = function () {
             }
         }
     };
-    xhr.open('GET', "../index.json");
+    xhr.open("GET", "https://danpark13.github.io/index.json");
+    // xhr.open('GET', "../index.json");
     xhr.send();
 }
 
